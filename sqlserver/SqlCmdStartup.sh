e129cfd4882cb72a33030ad92c5c79063dea0dfb
Liam Beeton <liam.beeton@wintechnologies.net>
2020-01-06 12:45:23 +0200

    fix: ignore first row on import

--- sqlserver/SqlCmdStartup.sh
+++ sqlserver/SqlCmdStartup.sh
@@ -12,7 +12,7 @@ sleep 10s
 # /opt/mssql-tools/bin/bcp GeoLite2.dbo.CityLocation format nul -S localhost -U sa -P Passw0rd -c -f CityLocation-c.fmt
 
 # import GeoLite2 City IPv4 Addresses
-/opt/mssql-tools/bin/bcp GeoLite2.dbo.CityBlock in ./geolite2/GeoLite2-City-Blocks-IPv4.csv -c -t ',' -r '0x0A' -S localhost -U sa -P Passw0rd
+/opt/mssql-tools/bin/bcp GeoLite2.dbo.CityBlock in ./geolite2/GeoLite2-City-Blocks-IPv4.csv -c -t ',' -r '0x0A' -F 2 -S localhost -U sa -P Passw0rd
 
 # import GeoLite2 City Locations
-/opt/mssql-tools/bin/bcp GeoLite2.dbo.CityLocation in ./geolite2/GeoLite2-City-Locations-en.csv -c -t ',' -r '0x0A' -S localhost -U sa -P Passw0rd
+/opt/mssql-tools/bin/bcp GeoLite2.dbo.CityLocation in ./geolite2/GeoLite2-City-Locations-en.csv -c -t ',' -r '0x0A' -F 2 -S localhost -U sa -P Passw0rd
