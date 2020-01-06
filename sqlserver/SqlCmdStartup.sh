f69e799fd861bb3797f4d2690a58ffe662f36377
Liam Beeton <liam.beeton@wintechnologies.net>
2020-01-06 10:10:43 +0200

    feat: add format file support for import

--- sqlserver/SqlCmdStartup.sh
+++ sqlserver/SqlCmdStartup.sh
@@ -5,8 +5,14 @@ sleep 10s
 # run the script to create the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Passw0rd -d master -i ./SqlCmdScript.sql
 
+# create Format file for GeoLite2 City IPv4 Addresses
+/opt/mssql-tools/bin/bcp GeoLite2.dbo.CityBlock format nul -S localhost -U sa -P Passw0rd -c -f CityBlock-c.fmt
+
+# create Format file for GeoLite2 City Locations
+/opt/mssql-tools/bin/bcp GeoLite2.dbo.CityLocation format nul -S localhost -U sa -P Passw0rd -c -f CityLocation-c.fmt
+
 # import GeoLite2 City IPv4 Addresses
-/opt/mssql-tools/bin/bcp CityBlock in ./GeoLite2-City-Blocks-IPv4.csv -S localhost -U sa -P Passw0rd -d GeoLite2 -c -t ',' -r '0x0A'
+/opt/mssql-tools/bin/bcp CityBlock in ./geolite2/GeoLite2-City-Blocks-IPv4.csv -f ./CityBlock-c.fmt -S localhost -U sa -P Passw0rd -d GeoLite2 -c -t ',' -r '0x0A'
 
 # import GeoLite2 City Locations
-/opt/mssql-tools/bin/bcp CityLocation in ./GeoLite2-City-Locations-en.csv -S localhost -U sa -P Passw0rd -d GeoLite2 -c -t ',' -r '0x0A'
+/opt/mssql-tools/bin/bcp CityLocation in ./geolite2/GeoLite2-City-Locations-en.csv -f ./CityLocation-c.fmt -S localhost -U sa -P Passw0rd -d GeoLite2 -c -t ',' -r '0x0A'
